bc1133c73e7dee86fa7448196e22100bf25b5e69
Aaron Boswell <aaronboswell@Aarons-MacBook-Pro.local>
2017-09-11 17:10:47 -0400

    Initial Commit

---
+++ Something/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Something
+//
+//  Created by Aaron Boswell on 2017-09-11.
+//  Copyright © 2017 Aaron Boswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
