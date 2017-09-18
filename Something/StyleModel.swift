4c236f725781e1d40f53488c7ebc5d95c9ce083a
fhboswell <franklin.henry.boswell@gmail.com>
2017-09-17 23:46:14 -0700

    changing style

--- Something/StyleModel.swift
+++ Something/StyleModel.swift
@@ -14,11 +14,21 @@ class StyleModel  {
     static let sharedInstance = StyleModel()
 
     
-    var backgroundColor = UIColor(rgb:0x4286f4)
+    var backgroundColor = UIColor(rgb:0x1985A1)
+    
+   //easy blue UIColor(rgb:0x1985A1)
+    //forest green UIColor(rgb:0x09814A)
+    
+   
+    //greenblue UIColor(rgb:0x2A9D8F)
+    //pastel sky UIColor(rgb:0x4286f4)
     //puke green: UIColor(rgb:0x69D300)
     
-    var labelColor = UIColor(rgb:0x070600)
-    var buttonColor = UIColor(rgb:0x7A306C)
+    var labelColor = UIColor(rgb:0x070600)    //sharp black
+    var buttonColor = UIColor(rgb:0x393F42)  //impact grey
+    
+    //candy purple UIColor(rgb:0x7A306C)
+    
     
     var kindColors: [String: Int] = ["need to do": 0xF9CB40,
                                    "nice to do": 0xEA526F,
@@ -30,4 +40,10 @@ class StyleModel  {
                                      "60min": 0xBCED09,
                                      "Doesn't matter": 0xACF39D]
 
+    /* Serbert scheme
+ ["5min": 0xF9CB40,
+ "30min": 0xEA526F,
+ "60min": 0xBCED09,
+ "Doesn't matter": 0xACF39D]
+ */
 }
